cd28805a979f1d887b004e0fb0173f5da2749e72
Natnael_Yigzaw <127339716+Natnael-Yigzaw@users.noreply.github.com>
2024-01-09 10:58:27 +0000

    refactored home and app

--- app.js
+++ app.js
@@ -1,31 +1,56 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const ejs = require("ejs");
-require("./config/connection");
-require("mongoose");
-require("./model/Post");
+const _ = require("lodash");
 
+const aboutContent =
+  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent =
+  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const posts = [];
 const app = express();
 
 app.set("view engine", "ejs");
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// Import routes
-const homeRoutes = require("./routes/homeRoutes");
-const postRoutes = require("./routes/postRoutes");
-const composeRoutes = require("./routes/composeRoutes");
-const aboutRoutes = require("./routes/aboutRoutes");
-const contactRoutes = require("./routes/contactRoutes");
+app.get("/", (req, res) => {
+  res.render("home", { posts: posts });
+});
 
-// Use the imported routes
-app.use("/", homeRoutes);
-app.use("/posts", postRoutes);
-app.use("/compose", composeRoutes);
-app.use("/about", aboutRoutes);
-app.use("/contact", contactRoutes);
+app.get("/posts/:postname", (req, res) => {
+  const postParamameter = _.lowerCase(req.params.postname);
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+  posts.forEach(function (post) {
+    const storedTitle = _.lowerCase(post.title);
+    if (postParamameter === storedTitle) {
+      res.render("post", { posttitle: post.title, postcontent: post.content });
+    }
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { aboutdescription: aboutContent });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", { contactdescription: contactContent });
+});
+
+app.get("/compose", (req, res) => {
+  res.render("compose");
+});
+
+app.post("/compose", (req, res) => {
+  const post = {
+    title: req.body.posttitle,
+    content: req.body.postbody,
+  };
+  posts.push(post);
+  res.redirect("/");
+});
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
 });
